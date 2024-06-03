b244bf47e2a0e3d8f586db04aa360abe79bd93fa
Maxime Bertrait <31846522+mbertrait@users.noreply.github.com>
2024-06-03 14:53:00 +0200

    Add kernel extraction and application with PyTorch implem  (unfold and bmm)

--- grappa_3D.py
+++ grappa_3D.py
@@ -1,175 +1,156 @@
 import torch
-import torch.nn as nn
+import math
+
 from tqdm import tqdm
+from typing import Any, Union, Tuple, List
 
 
-def pinv(M, lmda=0.01):
-    regularizer = lmda * torch.linalg.norm(M) * torch.eye(M.shape[0])
-    reg_pinv = torch.linalg.pinv(M @ M.conj().T + regularizer)
-    return reg_pinv
+def fixShapeAndIFFT(sig):
+    sig_r = torch.zeros(32,400,400,320, dtype=torch.complex64)
+    sig_r[:, :, 50:350, 1:320] = sig
+
+    for nc in range(sig.shape[0]):
+        sig_r[nc] = torch.fft.fftshift(torch.fft.ifft(torch.fft.ifft(torch.fft.ifft(torch.fft.fftshift(sig_r[nc], dim=(0,1,2)), dim=0), dim=1), dim=2), dim=(0,1,2))
+
+    return sig_r
 
 
-def grappa3D(sig, acs, afy, afz, window_size=None, grappa_kernel=None, cuda=False):
-    nc, nx, ny, nz = sig.shape
-    acsnc, acsnx, acsny, acsnz = acs.shape
-    sig_dtype = sig.dtype
-    af = afy*afz
+def reshape_fortran(x, shape):
+    if len(x.shape) > 0:
+        x = x.permute(*reversed(range(len(x.shape))))
+    return x.reshape(*reversed(shape)).permute(*reversed(range(len(shape))))
 
-    pat = torch.zeros(3*af+1, 3*af+1) if not window_size else torch.zeros(*window_size)
-    pat[::afy, ::afz] = 1
 
-    # Determine the target block size
-    tbly = int(af/sum(pat[:af, 0]))
-    tblz = int(af/sum(pat[0, :af]))
+# def pinv(M, lmda=1e-6):
+#     regularizer = lmda * torch.linalg.norm(M) * torch.eye(M.shape[0])
+#     reg_pinv = torch.linalg.pinv(M @ M.conj().T + regularizer)
+#     return reg_pinv
+
+def pinv_linalg(A, lmda=1e-2, cuda=False, lanczos=True):
+    #TODO: Let user select backeng (numpy, scipy, torch (cuda or not))
+    if cuda:
+        A = A.cuda()
+    AA = A.conj().T@A
+    S = torch.linalg.eigvalsh(AA)[-1].item() # Largest eigenvalue
+    lambda_sq = (lmda**2) * abs(S)
+    I = torch.eye(AA.shape[0], dtype=A.dtype)
+
+    regularized_matrix = AA + I * lambda_sq
+
+    A_pinv = torch.linalg.solve(regularized_matrix, A.conj().T)
+    return A_pinv.cpu() if cuda else A_pinv
+
+
+
+def grappa3D(
+        sig: torch.Tensor,
+        acs: torch.Tensor,
+        af: Union[list[int], tuple[int, ...]],
+        block_size=None,
+        grappa_kernel=None,
+        cuda=True,
+        verbose=True
+) -> torch.Tensor:
+    
+    nc, ny, nz, nx = sig.shape
+    acsny, acsnz, acsnx = acs.shape[1:]
+
+    total_af = math.prod(af)
+
+    #TODO: Might need to do checks on arguments passed to the function
+
+    # Generate the acceleration pattern
+    #TODO: Will need to think about `block_size` argument that can be given by the user
+    pat = torch.zeros(*(3*a+1 for a in af))
+    slices = [slice(None, None, a) for a in af]
+    pat[tuple(slices)] = 1
+
+    # Determine the target block size from total_af
+    tbly = int(total_af/sum(pat[:total_af, 0]))
+    tblz = int(total_af/sum(pat[0, :total_af]))
+    tblx=1
 
     # Determine 3D source block size
-    sblx = min(5, acsnx)
-    sbly = min(max(af, 3*tbly), acsny)
-    sblz = min(max(af, 3*tblz), acsnz)
+    sblx = min(7, acsnx)
+    sbly = min(max(total_af, 3*tbly), acsny)
+    sblz = min(max(total_af, 3*tblz), acsnz)
 
     xpos = (sblx-1)//2
     ypos = (sbly-tbly)//2
     zpos = (sblz-tblz)//2
 
-    # indexes of sources points in the window
-    idxs_src = pat[:sbly, :sblz] == 1
-    #idxs_src = idxs_src.unsqueeze(0).repeat(sblx,1,1).reshape(-1)
+    idxs_src = (pat[:sbly, :sblz] == 1)
+    idxs_src = idxs_src.unsqueeze(-1).expand(*idxs_src.size(), sblx)
+    idxs_srcT = (pat[:sbly, :sblz].T == 1).flatten()
 
-    idxs_src = torch.stack([idxs_src] * sblx).reshape(-1)
+    idxs_tgs = torch.zeros(sbly, sblz, sblx)
+    idxs_tgs[ypos:ypos+tbly, zpos:zpos+tblz, xpos:xpos+1] = 1
+    idxs_tgs = (idxs_tgs == 1)
+
     nsp = idxs_src.sum()
 
     if grappa_kernel is None:
 
-
         ########################################################
         #                  Kernel estimation                   #
         ########################################################
+        if verbose:
+            print("Kernel estimation...")
 
 
-        n_shift_x = acsnx - sblx + 1
-        n_shift_y = acsny - sbly + 1
-        n_shift_z = acsnz - sblz + 1
+        windows = acs.unfold(dimension=1, size=sbly, step=1)
+        windows = windows.unfold(dimension=2, size=sblz, step=1)
+        windows = windows.unfold(dimension=3, size=sblx, step=1)
+        windows = windows.permute(-1,-2,-3,0,1,2,3)
 
-        src = torch.zeros(nc*nsp, n_shift_x*n_shift_y*n_shift_z, dtype=sig_dtype)
-        tgs = torch.zeros(nc*tbly*tblz, n_shift_x*n_shift_y*n_shift_z, dtype=sig_dtype)
-
-        for shift_x in tqdm(range(n_shift_x)):
-            for shift_y in range(n_shift_y):
-                for shift_z in range(n_shift_z):
-
-                    s = acs[:, shift_x:shift_x + sblx,
-                            shift_y:shift_y + sbly,
-                            shift_z:shift_z + sblz].reshape(nc, -1)
-                    s = s[:, idxs_src].reshape(-1)
-
-                    t = acs[:, shift_x + xpos:shift_x + xpos + 1,
-                            shift_y + ypos:shift_y + ypos + tbly,
-                            shift_z + zpos:shift_z + zpos + tblz].reshape(-1)
-                                    
-                    src[...,shift_x+shift_y+shift_z] = s
-                    tgs[...,shift_x+shift_y+shift_z] = t
-
+        src = windows[idxs_src.permute(2,1,0)].reshape(nc*nsp, -1)
+        tgs = windows[idxs_tgs.permute(2,1,0)].reshape(nc*tbly*tblz*tblx, -1)
         
-        grappa_kernel = tgs @ src.conj().T @ pinv(src)
-        torch.save(grappa_kernel, "grappa_k.torch")
+        if cuda:
+            grappa_kernel = tgs @ pinv_linalg(src.T, cuda=cuda).T
 
     ########################################################
     #                  Kernel application                  #
     ########################################################
 
-    # Zero-Pad the signal
-    sig = nn.functional.pad(sig, (sblz//2 - tblz//2, sblz//2 - tblz//2, sbly//2 - tbly//2, sbly//2 - tbly//2, sblx//2, sblz//2))
-    nx_pad, ny_pad, nz_pad = sig.shape[1:]
+    grappa_kernel = reshape_fortran(grappa_kernel, (nc * tbly * tblz, nc * nsp // sblx, sblx))
+    grappa_conv = torch.zeros(nc * tbly * tblz, nc * nsp // sblx, nx, dtype=sig.dtype, device=sig.device)
+    start_idx = nx // 2 - sblx // 2
+    end_idx = start_idx + sblx
 
-    n_shift_x = nx_pad - sblx + 1
-    n_shift_y = ny_pad - sbly + 1
-    n_shift_z = nz_pad - sblz + 1
-    """
-    for shift_x in tqdm(range(n_shift_x)):
-        for shift_y in range(0, n_shift_y, tbly):
-            for shift_z in range(0, n_shift_z, tblz):
-                    s = sig[:, shift_x:shift_x + sblx,
-                               shift_y:shift_y + sbly,
-                               shift_z:shift_z + sblz].reshape(nc, -1)
-                    s = s[:, idxs_src].reshape(-1)
-                    t = sig[:, shift_x + xpos:shift_x + xpos + 1,
-                               shift_y + ypos:shift_y + ypos + tbly,
-                               shift_z + zpos:shift_z + zpos + tblz].reshape(-1)
-                    
-                    sig[:, shift_x + xpos:shift_x + xpos + 1,
-                           shift_y + ypos:shift_y + ypos + tbly,
-                           shift_z + zpos:shift_z + zpos + tblz]  = (grappa_kernel @ s.T).squeeze().reshape(nc, 1, tbly, tblz)
-    """
-    #n_shift_y, n_shift_z = sig.shape[2], sig.shape[3]  # assuming sig is a 4D tensor with shape (nc, ..., ..., ...)
-    n_shift_x = nx_pad - sblx + 1
-    n_shift_y = ny_pad - sbly + 1 - acsny
-    n_shift_z = nz_pad - sblz + 1 - acsnz
-    n_blocks = n_shift_x * (n_shift_y // tbly) * (n_shift_z // tblz)
+    grappa_conv[:, :, start_idx:end_idx] = torch.flip(grappa_kernel, [2])
+    
+    if sblx>1:
+        sig = torch.fft.ifftshift(torch.fft.ifft(torch.fft.fftshift(sig, dim=-1), dim=-1), dim=-1)
 
-    # Initialize the large S and T matrice
-    S = torch.zeros(nc * nsp, n_blocks, dtype=sig_dtype)
-    T = torch.zeros(nc * 1 * tbly * tblz, n_blocks, dtype=sig_dtype)
+    if nx > 1:
+        grappa_conv = torch.fft.ifftshift(torch.fft.ifft(torch.fft.fftshift(grappa_conv, dim=-1), dim=-1), dim=-1)
+        sig = torch.cat((sig[:, -af:, :, :], sig, sig[:, :af, :, :]), dim=1)
+    
+    if nz > 1:
+        sig = torch.cat((sig[:, :, -2*af:, :], sig, sig[:, :, :2*af, :]), dim=2)
 
-    #sig = sig.cuda()
-    #S = S.cuda()
-    #T = T.cuda()
+    shift_y=2
+    sig_tmp = torch.zeros_like(sig).cuda()
+    np = int(sum(idxs_srcT))
 
-    block_idx = 0
-    print("making blocks")
-    for shift_x in tqdm(range(n_shift_x)):
-        for shift_y in range(0, n_shift_y, tbly):
-            for shift_z in range(0, n_shift_z, tblz):
-                if not (ny_pad//2 - acsny//2 <= shift_y <= ny_pad//2 + acsny//2 and nz_pad//2 - acsnz//2 <= shift_z <= nz_pad//2 + acsnz//2):
-                    s = sig[:, shift_x:shift_x + sblx,
-                            shift_y:shift_y + sbly,
-                            shift_z:shift_z + sblz].reshape(nc, -1)
-                    s = s[:, idxs_src].reshape(-1)
-                    t = sig[:, shift_x + xpos:shift_x + xpos + 1,
-                            shift_y + ypos:shift_y + ypos + tbly,
-                            shift_z + zpos:shift_z + zpos + tblz].reshape(-1)
+    y_ival = range(shift_y, ny+2-af-sbly, afy)
+    z_ival = range(0, nz+4*af-sblz, afz)
+    grappa_conv = grappa_conv.cuda()
 
-                    S[:, block_idx] = s
-                    T[:, block_idx] = t
-                    block_idx += 1
+    for y in tqdm(y_ival):
+        acq_y = sig[:, y:y+sbly, :, :].cuda()
+        for z in z_ival:
+            acq = reshape_fortran(acq_y[:,:,z:z+sblz, :], (nc, sbly*sblz, nx))
+            acq = reshape_fortran(acq[:, idxs_srcT, :], (nc*np,1, nx))
+            sig_tmp[:,y+ypos:y+ypos+tbly, z+zpos:z+zpos+tblz, :] =  reshape_fortran(
+                                                                        torch.bmm(
+                                                                            grappa_conv.permute(2,0,1),
+                                                                            acq.permute(2,0,1)
+                                                                        ).permute(1,2,0),
+                                                                        (nc, tbly, tblz, nx)
+                                                                    )
 
     if cuda:
-        grappa_kernel = grappa_kernel.cuda()
-        S = S.cuda()
-        #sig = sig.cuda()
-    # Perform the giant matrix multiplication
-    result = grappa_kernel @ S.T
-    print("done")
-    result = result.cpu()
-
-    sig = sig.cuda()
-    # Place the result back into sig
-    block_idx = 0
-    for shift_x in tqdm(range(n_shift_x)):
-        for shift_y in tqdm(range(0, n_shift_y, tbly)):
-            for shift_z in range(0, n_shift_z, tblz):
-                result_block = result[:, block_idx].reshape(nc, 1, tbly, tblz)
-                sig[:, shift_x + xpos:shift_x + xpos + 1,
-                    shift_y + ypos:shift_y + ypos + tbly,
-                    shift_z + zpos:shift_z + zpos + tblz] = result_block
-                block_idx += 1
-
-
-
-
-# import mapvbvd
-# scan = mapvbvd.mapVBVD(r"/home/mb279022/meas_MID00231_FID25576_t2_spc_sag_iso0_55mm_p3x2.dat")
-# scan.image.squeeze = True
-# scan.image.flagRemoveOS = True
-# scan.image.flagDoAverage = True
-# scan.image.flagIgnoreSeg = True
-# sig = torch.from_numpy(scan.image[''])
-# sig = sig.permute(1,0,2,3)
-
-# scan.refscan.squeeze = True
-# scan.refscan.flagRemoveOS = True
-# scan.refscan.flagDoAverage = True
-# scan.refscan.flagIgnoreSeg = True
-# ref = torch.from_numpy(scan.refscan[''])
-# ref = ref.permute(1,0,2,3)
-
-# grappa3D(sig, ref, 3, 2, grappa_kernel=torch.load("grappa_k.torch"))
+        sig_tmp = sig_tmp.cpu()
+    return sig_tmp
